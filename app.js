a1bd93361cca560ed9b841a3851b0f9c5ecef391
Devin Pawline <devinpawline@outlook.com>
2021-07-19 10:00:40 -0500

    setting up connection in app.js

--- app.js
+++ app.js
@@ -2,4 +2,19 @@
 const inquirer = require("inquirer")
 const mysql = require("mysql")
 const cTable = require('console.table');
+const { connect } = require("http2");
 
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "0e5#97Dp"
+    database: "employee_trackerDB"
+});
+
+// Connection ID 
+connection.connect(function(err) {
+    if (err) throw err
+    console.log("Connected as ID " + connection.threadId)
+    startPrompt();
+});
